f6e60936fc82bb51230d544f8057d292de4ebff4
Teo <vanmeerhaegheteo@gmail.com>
2023-02-24 15:54:28 +0100

    Added fixture

--- src/Controller/Admin/AdminDashboardController.php
+++ src/Controller/Admin/AdminDashboardController.php
@@ -8,6 +8,17 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Articles;
+use App\Entity\Avis;
+use App\Entity\Client;
+use App\Entity\Contact;
+use App\Entity\Maps;
+use App\Entity\Quiz;
+use App\Entity\Rdv;
+use App\Entity\Reponses;
+use App\Entity\Style;
+use App\Entity\Tatoueur;
+
 class AdminDashboardController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'admin')]
@@ -25,6 +36,15 @@ class AdminDashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Articles', 'fas fa-list', Articles::class);
+        yield MenuItem::linkToCrud('Avis', 'fas fa-list', Avis::class);
+        yield MenuItem::linkToCrud('Clients', 'fas fa-list', Client::class);
+        yield MenuItem::linkToCrud('Contact', 'fas fa-list', Contact::class);
+        yield MenuItem::linkToCrud('Maps', 'fas fa-list', Maps::class);
+        yield MenuItem::linkToCrud('Quiz', 'fas fa-list', Quiz::class);
+        yield MenuItem::linkToCrud('Rendez-vous', 'fas fa-list', Rdv::class);
+        yield MenuItem::linkToCrud('Reponses aux quizs', 'fas fa-list', Reponses::class);
+        yield MenuItem::linkToCrud('Style de tatouage', 'fas fa-list', Style::class);
+        yield MenuItem::linkToCrud('Tatoueurs', 'fas fa-list', Tatoueur::class);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,25 +2,33 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use Faker\Generator;
+use App\Entity\Client;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
+
+    private Generator $faker;
+
+    public function __construct(){
+        $this->faker = Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager): void
     {
-        $tatoueur = new Tatoueur();
-        $tatoueur->setNom('Tatoueur #1')
-            ->setPrenom('test')
-            ->setMdp('test')
-            ->setMail('test')
-            ->setTel('0000000')
-            ->setAdresse('test')
-            ->setStyle('test')
-            ->setNomDuSalon('test')
-            ->setDescription('test');
-
-        $manager->persist($tatoueur);
+        for ($i=1; $i <= 50; $i++) { 
+            $client = new Client();
+            $client->setNom($this->faker->word())
+                ->setPrenom('test')
+                ->setMdp(mt_rand(0, 350))
+                ->setMail('test')
+                ->setTel('010208909');
+        
+                $manager->persist($client);
+        }
 
         $manager->flush();
     }
